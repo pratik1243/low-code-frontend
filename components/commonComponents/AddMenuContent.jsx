1783dca5a1e46d1bd4fac335c0b7ebeacbb0bf68
pratik1243 <87714664+pratik1243@users.noreply.github.com>
2025-11-27 00:51:02 +0530

    Menu null changes

--- components/commonComponents/AddMenuContent.jsx
+++ components/commonComponents/AddMenuContent.jsx
@@ -178,6 +178,7 @@ function AddMenuContent({ menuIndex, setMenuIndex }) {
           <Button
             className="go-back-btn"
             onClick={() => {
+              setMenuIndex(null);
               setIsSubMenuOpen(false);
             }}
           >
