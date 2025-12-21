06752296a30ede60599c1e37b7ba97c64f4d57c8
pratik1243 <thakurpratik226@gmail.com>
2025-12-21 14:30:45 +0530

    breakpoints changes in web page

--- components/WebPage.jsx
+++ components/WebPage.jsx
@@ -86,21 +86,20 @@ const WebPage = () => {
   };
 
   useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  useEffect(() => {
-    handleResize();
     const link = document.createElement("link");
     link.href = `https://fonts.googleapis.com/css2?family=${selectedFont?.replace(/ /g, "+")}:wght@400&display=swap`;
     link.rel = "stylesheet";
     document.body.style.backgroundColor = pageBackground;
     document.head.appendChild(link);
+    
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
     return () => {
       document.head.removeChild(link);
+      window.removeEventListener("resize", handleResize);
     };
-  }, []);
+  }, [selectedFont]);
   
   useEffect(() => {
     fetchPage(breakPoint);
