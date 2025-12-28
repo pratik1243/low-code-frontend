7ba271262514a19a7285bf81456121b03327f645
pratik1243 <thakurpratik226@gmail.com>
2025-12-28 15:17:27 +0530

    no changes

--- components/fieldsComponents/Divider.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Divider = ({ ele, path }) => {
-  return (
-    <div>{path.includes("web-page") ? <hr/> : "Divider"}</div>
-  )
-}
-
-export default Divider
--- src/app/register/page.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Register from "../../../components/Register";
-
-const registerPage = () => {
-  return <Register />;
-};
-
-export default registerPage;
