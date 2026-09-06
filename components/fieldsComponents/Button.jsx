7bae64598123cb1eda1cb06b0b640accd31c2326
pratik1243 <87714664+pratik1243@users.noreply.github.com>
2026-09-06 19:39:34 +0530

    Update Button.jsx
    Button CradBox chnages 2

--- components/fieldsComponents/Button.jsx
+++ components/fieldsComponents/Button.jsx
@@ -12,12 +12,12 @@ import { commonPostApiFunction } from "../../services/commonApiFunc";
 import { addPixel, errorMessageFunc } from "../../utils/customizePropFunctions";
 import { snackProps } from "../../utils/customizeOptions";
 
-const ButtonComp = ({ ele, path }) => {
+const ButtonComp = ({ ele, path, mainIndex = null, cardInnerIndex = null }) => {
   const router = useRouter();
   const dispatch = useDispatch();
   const isWebPage = path.includes("web-page");
   const { forms, setForms, breakPoint } = useContext(
-    isWebPage ? PageContext : FormContext
+    isWebPage ? PageContext : FormContext,
   );
   const fieldArray = ele?.props?.fields.map((el) => el?.value);
 
@@ -25,8 +25,8 @@ const ButtonComp = ({ ele, path }) => {
     return typeof data?.props?.value == "object"
       ? data?.props?.value?.map((e) => e?.value)
       : data?.props?.checked
-      ? data?.props?.checked
-      : data?.props?.value;
+        ? data?.props?.checked
+        : data?.props?.value;
   };
 
   const sendEmailNotification = async (
@@ -34,7 +34,7 @@ const ButtonComp = ({ ele, path }) => {
     receiver_email,
     subject,
     title,
-    content
+    content,
   ) => {
     try {
       dispatch(setLoader(true));
@@ -66,87 +66,121 @@ const ButtonComp = ({ ele, path }) => {
     let mailText = "";
     const formData = {};
 
-    const validateForms = forms[breakPoint].map((el, i) => {
-      const nestedForm = el?.content?.map((eles, id) => {
-        if (fieldArray.includes(eles?.props?.name)) {
-          formData[eles?.props?.name] = getFieldValue(eles);
-          if (
-            errorMessageFunc(
-              eles,
-              eles?.props?.checked || eles?.props?.value
-            ) !== ""
-          ) {
-            isFieldsInvalid = true;
-          }
-          return {
-            ...eles,
-            form: {
-              ...eles?.form,
-              error_message: errorMessageFunc(
-                eles,
-                eles?.props?.checked || eles?.props?.value
-              ),
-            },
-          };
-        }
-        return eles;
-      });
-
-      if (el?.content) {
-        return { ...el, content: nestedForm };
-      } else {
-        if (fieldArray.includes(el?.props?.name)) {
-          formData[el?.props?.name] = getFieldValue(el);
-          if (
-            errorMessageFunc(el, el?.props?.checked || el?.props?.value) !== ""
-          ) {
-            isFieldsInvalid = true;
-          }
-          return {
-            ...el,
-            form: {
-              ...el?.form,
-              error_message: errorMessageFunc(
-                el,
-                el?.props?.checked || el?.props?.value
-              ),
-            },
-          };
-        }
+    const processField = (field) => {
+      const name = field?.props?.name;
+      if (!name || !fieldArray.includes(name)) {
+        return field;
       }
-      return el;
+      const value = getFieldValue(field);
+      const validationValue =
+        field?.props?.checked ?? field?.props?.value ?? value;
+      const errorMessage = errorMessageFunc(field, validationValue);
+      if (errorMessage !== "") {
+        isFieldsInvalid = true;
+      }
+      if (
+        formData[name] === undefined ||
+        (value !== "" && value !== null && value !== undefined)
+      ) {
+        formData[name] = value;
+      }
+      return {
+        ...field,
+        form: {
+          ...field?.form,
+          error_message: errorMessage,
+        },
+      };
+    };
+
+    const validateForms = forms[breakPoint].map((el, i) => {
+      const nestedForm = el?.content?.map((eles) => {
+        return processField(eles);
+      });
+      const cardContentForm = el?.props?.cards?.map((data) => {
+        const updatedForm = data?.content?.map((datas) => {
+          const cardNestedForm = datas?.content?.map((eles) => {
+            return processField(eles);
+          });
+
+          if (datas?.content) {
+            return {
+              ...datas,
+              content: cardNestedForm,
+            };
+          }
+          return processField(datas);
+        });
+        if (data && data?.id === cardInnerIndex) {
+          return {
+            ...data,
+            content: updatedForm,
+          };
+        }
+        const dataIndex = el?.props?.cards?.indexOf(data);
+        if (dataIndex === cardInnerIndex) {
+          return {
+            ...data,
+            content: updatedForm,
+          };
+        }
+        return data;
+      });
+      if (el?.type === "container" && mainIndex === i) {
+        return {
+          ...el,
+          content: nestedForm,
+        };
+      }
+      if (el?.type === "card_box" && mainIndex === i) {
+        return {
+          ...el,
+          props: {
+            ...el.props,
+            cards: cardContentForm,
+          },
+        };
+      }
+      return processField(el);
     });
+    if (fieldArray.length > 0) {
+      setForms({
+        ...forms,
+        [breakPoint]: validateForms,
+      });
+    }
     if (ele?.props?.external_link) {
       router.push(ele?.props?.external_link);
-    } else if (ele?.props?.redirectUrl?.page_route) {
+      return;
+    }
+    if (ele?.props?.redirectUrl?.page_route) {
       router.push(ele?.props?.redirectUrl?.page_route);
-    }
-    if (fieldArray.length > 0) {
-      setForms({ ...forms, [breakPoint]: validateForms });
-    }
+      return;
+    }    
     if (!isFieldsInvalid) {
+      const emailProps = ele?.props?.emailSendProps;
       if (
-        ele?.props?.emailSendProps?.sender_email &&
-        ele?.props?.emailSendProps?.receiver_email &&
-        ele?.props?.emailSendProps?.subject
+        emailProps?.sender_email &&
+        emailProps?.receiver_email &&
+        emailProps?.subject
       ) {
-        const contentArr =
-          ele?.props?.emailSendProps?.content?.htmlContent?.split("{{");
-        contentArr.map((el, i) => {
-          if (el?.includes("}}")) {
-            mailText += `${formData[el?.split("}}")[0]]} ${
-              el?.split("}}")[1] || ""
-            }`;
+        const htmlContent = emailProps?.content?.htmlContent || "";
+        const contentArr = htmlContent.split("{{");
+        contentArr.forEach((content) => {
+          if (content?.includes("}}")) {
+            const [fieldName, remainingText] = content.split("}}");
+            const fieldValue = formData[fieldName] ?? "";
+            mailText += `${fieldValue}${remainingText || ""}`;
           } else {
-            mailText += el;
+            mailText += content;
           }
         });
         sendEmailNotification(
-          ele?.props?.emailSendProps?.sender_email,
-          ele?.props?.emailSendProps?.receiver_email,
-          ele?.props?.emailSendProps?.subject,
-          ele?.props?.emailSendProps?.title,
-          mailText
+          emailProps.sender_email,
+          emailProps.receiver_email,
+          emailProps.subject,
+          emailProps.title,
+          mailText,
         );
       }
     }
